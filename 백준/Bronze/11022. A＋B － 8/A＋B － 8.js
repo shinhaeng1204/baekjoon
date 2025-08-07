51a9ce10c593202a56b4eff4679db20aeb54f2b5
shinhaeng1204 <129822507+shinhaeng1204@users.noreply.github.com>
2025-08-07 13:15:10 +0900

    [Bronze V] Title: A+B - 8, Time: 96 ms, Memory: 9536 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const test = Number(input[0]);
+
+for(let i=1;i<test+1;i++){
+    const [num1, num2] = input[i].split(" ").map(Number);
+    let sum=num1+num2;
+    console.log(`Case #${i}: ${num1} + ${num2} = ${sum}`)
+}
