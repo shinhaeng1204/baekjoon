8abbadf4e21a2f6fc20cb11b868f39fc800fc4e8
shinhaeng1204 <129822507+shinhaeng1204@users.noreply.github.com>
2025-08-07 13:14:38 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const test = Number(input[0]);
+
+for(let i=1;i<=test;i++){
+    console.log(" ".repeat(test-i)+"*".repeat(i));
+}
