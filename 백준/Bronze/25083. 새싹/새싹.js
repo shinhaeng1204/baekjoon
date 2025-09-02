38b0f0adf65c82b82e5611af2616e3cd59790135
shinhaeng1204 <129822507+shinhaeng1204@users.noreply.github.com>
2025-09-02 11:49:07 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8');
+
+console.log("         ,r'\"7")
+console.log("r`-_   ,'  ,/")
+console.log(" \\. \". L_r'")
+console.log("   `~\\/")
+console.log("      |")
+console.log("      |")
+
