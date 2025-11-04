7a20fb24e85c7dac15f6fe6e12c698933332d0a8
shinhaeng1204 <129822507+shinhaeng1204@users.noreply.github.com>
2025-11-04 13:47:26 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1988 ms, Memory: 200380 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+
+const n = Number(input[0])
+let arr = new Array()
+
+for(let i=1;i<=n;i++){
+    arr.push(input[i])
+}
+arr.sort((a,b)=>a-b)
+console.log(arr.join("\n"))
